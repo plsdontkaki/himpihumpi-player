408f9ba726404b5f8ead9626e58b1985a840b1ae
plsdontkaki <168557308+plsdontkaki@users.noreply.github.com>
2026-06-13 14:48:33 +0200

    s

--- songlist-x.js
+++ songlist-x.js
@@ -2,43 +2,43 @@ let All_song = [
     {
         name: "1. ANUBIS! (feat. Sampxl & MrBear)",
         path: "music/zona-x/anubis.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "Sampxl, MrBear"
     },
     {
         name: "2. Terrorista (feat. Sampxl & Mrbear)",
         path: "music/zona-x/terrorista.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "Sampxl, Mrbear"
     },
     {
         name: "3. Himpihumpi (feat. K3t & Zoyrxq)",
         path: "music/zona-x/himpihumpi.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "K3t, Zoyrxq"
     },
     {
         name: "4. Viking (feat. K3t)",
         path: "music/zona-x/viking.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "K3t"
     },
     {
         name: "5. Zenevideo (feat. Kovix & Lil Ragu)",
         path: "music/zona-x/videozene.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "Kovix, Lil Ragu"
     },
     {
         name: "6. Pulzáló Ritmusok (feat. Kovix & Lil Ragu)",
         path: "music/zona-x/pulzaloritmusok.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "Kovix, Lil Ragu"
     },
     {
         name: "7. Titkos (feat. K3t)",
         path: "music/zona-x/titkos.mp3",
-        img: "music/zona-x.jpeg",
+        img: "/zona-x/zona-x.jpeg",
         singer: "K3t"
     }
 ];

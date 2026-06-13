dccd45614f06ebd010f971b7723f41705f660e6d
plsdontkaki <168557308+plsdontkaki@users.noreply.github.com>
2026-06-13 14:54:25 +0200

    s

--- zona-y-deluxe/songlist-y-deluxe.js
+++ zona-y-deluxe/songlist-y-deluxe.js
@@ -1,55 +1,55 @@
 let All_song = [
     {
         name: "1. TERRORISTÁK VANNAK A HÁZAMBAN (feat. Csillog)",
-        path: "music/zona-y-deluxe/terroristak.mp3",
+        path: "../music/zona-y-deluxe/terroristak.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Csillog"
     },
     {
         name: "2. OKIE (feat. Kattos)",
-        path: "music/zona-y-deluxe/okie.mp3",
+        path: "../music/zona-y-deluxe/okie.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Kattos"
     },
     {
         name: "3. Terrortámadás (feat. Csillog)",
-        path: "music/zona-y-deluxe/terrortamadas.mp3",
+        path: "../music/zona-y-deluxe/terrortamadas.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Csillog"
     },
     {
         name: "4. FASZXDDDDÍ (feat. Kattos)",
-        path: "music/zona-y-deluxe/faszxd.mp3",
+        path: "../music/zona-y-deluxe/faszxd.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Kattos"
     },
     {
         name: "5. Erdő (feat. Csillog)",
-        path: "music/zona-y-deluxe/erdo.mp3",
+        path: "../music/zona-y-deluxe/erdo.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Csillog"
     },
     {
         name: "6. JANESZKOOOO (feat. Kattos & Zylin)",
-        path: "music/zona-y-deluxe/janeszko.mp3",
+        path: "../music/zona-y-deluxe/janeszko.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Kattos & Zylin"
     },
     {
         name: "7. Szia ;D (feat. Kattos)",
-        path: "music/zona-y-deluxe/szia.mp3",
+        path: "../music/zona-y-deluxe/szia.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Kattos"
     },
     {
         name: "8. Atom (interlude) (feat. Bemondó Bácsi)",
-        path: "music/zona-y-deluxe/atom.mp3",
+        path: "../music/zona-y-deluxe/atom.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Bemondó Bácsi"
     },
     {
         name: "9. HÍV A KORMÁNY (VISZLÁT) (feat. Kattos, Nokia Phone & Magyar Kormány)",
-        path: "music/zona-y-deluxe/hivakormany.mp3",
+        path: "../music/zona-y-deluxe/hivakormany.mp3",
         img: "/zona-y-deluxe/zona-y-deluxe.jpg",
         singer: "Kattos, Nokia Phone & Magyar Kormány"
     },
--- zona-y/songlist-y.js
+++ zona-y/songlist-y.js
@@ -1,67 +1,67 @@
 let All_song = [
     {
         name: "1. OTP (feat. Kattos, Csillog, Halozat & Zylin)",
-        path: "music/zona-y/otp.mp3",
+        path: "../music/zona-y/otp.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos, Csillog, Halozat, Zylin"
     },
     {
         name: "2. XxX_b0mb4_XxX (feat. Kattos & Halozat)",
-        path: "music/zona-y/bomba.mp3",
+        path: "../music/zona-y/bomba.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos, Halozat"
     },
     {
         name: "3. TERRORISTA666 (feat. Kattos)",
-        path: "music/zona-y/terrorista.mp3",
+        path: "../music/zona-y/terrorista.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos"
     },
     {
         name: "4. KASZINÓ (feat. Zylin)",
-        path: "music/zona-y/kaszino.mp3",
+        path: "../music/zona-y/kaszino.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Zylin"
     },
     {
         name: "5. OTL RANT (feat. Kattos)",
-        path: "music/zona-y/otlrant.mp3",
+        path: "../music/zona-y/otlrant.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos"
     },
     {
         name: "6. VIDEÓZENE (feat. Kovix, Lil Ragu, Zylin & Sas)",
-        path: "music/zona-y/videozene.mp3",
+        path: "../music/zona-y/videozene.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kovix, Lil Ragu, Zylin, Sas"
     },
     {
         name: "7. Bladesesxdsgsddgfd_142_fmin (feat. Csillog, Kattos & Halozat)",
-        path: "music/zona-y/blade.mp3",
+        path: "../music/zona-y/blade.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Csillog, Kattos, Halozat"
     },
     {
         name: "8. xXx_H1mp1gang_xXx (feat. Kattos & Zoyraq)",
-        path: "music/zona-y/himpigang.mp3",
+        path: "../music/zona-y/himpigang.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos, Zoyraq"
     },
     {
         name: "9. MINECRAFTCORE (feat. Csillog, Kattos, Zylin & Halozat)",
-        path: "music/zona-y/minecraftcore.mp3",
+        path: "../music/zona-y/minecraftcore.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Csillog, Kattos, Zylin, Halozat"
     },
     {
         name: "10. H1mp1_C0r3 (feat. Kattos, Peter Griffin, Hatoska, Kovix, Zoyraq, Lil Ragu & Woodcraft.hu)",
-        path: "music/zona-y/himpicore.mp3",
+        path: "../music/zona-y/himpicore.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos, Peter Griffin, Hatoska, Kovix, Zoyraq, Lil Ragu, Woodcraft.hu"
     },
     {
         name: "11. Serleg (feat. Kattos, Halozat, Csillog & Zylin)",
-        path: "music/zona-y/serleg.mp3",
+        path: "../music/zona-y/serleg.mp3",
         img: "/zona-y/zona-y.jpeg",
         singer: "Kattos, Halozat, Csillog, Zylin"
     }
